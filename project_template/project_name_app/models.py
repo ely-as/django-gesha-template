a555a114bc3bb19a54a18bb68f7844b4609d8378
ely-as <mail@ely.as>
2023-06-10 15:59:22 -0700

    Template: create Enquiry model

--- project_template/project_name_app/models.py
+++ project_template/project_name_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Enquiry(models.Model):
+    name = models.CharField(_("Name"), max_length=255)
+    email = models.EmailField(_("Email address"))
+    phone_number = models.CharField(_("Phone number"), max_length=17)
+    description = models.TextField(_("Description"))

4dc5ee28ee9974627b42cfb7bdf5c3401683d1cb
ely-as <mail@ely.as>
2023-06-10 15:44:19 -0700

    Template: load dist files in template

--- project_template/project_name_app/views.py
+++ project_template/project_name_app/views.py
@@ -4,3 +4,10 @@ from django.views.generic import TemplateView
 
 class IndexView(gesha.JSContextMixin, TemplateView):
     template_name = "{{ project_name }}_app/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        projname = "{{ project_name }}"
+        context["script_path"] = f"{projname}_app/dist/{projname}.bundle.min.js"
+        context["stylesheet_path"] = f"{projname}_app/dist/main.css"
+        return context

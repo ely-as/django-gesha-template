d6b3cd30ac7c62592543cad3b028bf227904eea9
ely-as <mail@ely.as>
2023-06-19 13:49:17 -0700

    Template: install django-gesha via npm and create single bundle

--- project_template/src/main.ts
+++ project_template/src/main.ts
@@ -1,9 +1,10 @@
+import "django-gesha";
+
 async function handleEnquiry(e: Event) {
   const form = document.getElementById("enquiry-form") as HTMLFormElement;
   const formFields = form.querySelector(".form-fields");
   const formData = new FormData(form);
-  // TODO: fix window assertion by loading django-gesha types
-  const url = (window as any).django.reverse("enquiry");
+  const url = window.django.reverse("enquiry");
   const response = await fetch(url, {
     method: "POST",
     mode: "same-origin",
